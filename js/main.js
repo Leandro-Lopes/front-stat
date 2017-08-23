3beb3c7935e25bff02aa74eb081167cd09e45a80
Leandro Lopes <leeandro.sccp@gmail.com>
2017-08-23 14:47:44 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var app = angular.module('myapp',[]);
+
+app.controller('tabelasController',function($scope,$http){
+	$scope.dados=[];
+	$scope.ligas = [];
+	 $http.get("https://apistat.herokuapp.com/api/tabelas/")
+    .then(function(response) {
+        $scope.dados = response.data;
+    });
+     $http.get("https://apistat.herokuapp.com/api/ligas/")
+    .then(function(response) {
+        $scope.ligas = response.data;
+    });
+
+})
